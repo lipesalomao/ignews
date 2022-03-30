c9f1808994c2aa9229d8c9ed3dd7c9c3276ee091
Felipe Salomão <felipe@directon.com.br>
2022-03-29 22:23:34 -0300

    consumo da api do stripe com SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
+//import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import styles from './home.module.scss';
 import Head from "next/head";
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
 
-export default function Home() {
+// Client-sede rendering
+// Server-side rendering
+// Static Site Generation
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+    
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -15,12 +30,41 @@ export default function Home() {
           <h1>News about the <span>React </span>world.</h1>
           <p>
             Get access to all the puclications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton/>
+          <SubscribeButton priceId={product.priceId}/>
         </section>
         <img src="/images/avatar.svg" alt="girl coding" />
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1KiTXkC9MguhEOI5f3hm6MdT', {
+    expand: ['product'],
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US',{
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100)
+        
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 1 day
+  }
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return {
+
+  }
 }
